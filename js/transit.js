20e090d16f273d492ba6690ef2e223fb5fead7a8
u17024782 <u17024782@tuks.co.za>
2020-07-19 20:56:31 +0200

    A

---
+++ js/transit.js
@@ -0,0 +1,67 @@
+var map = L.map('map');
+
+
+if (hasWebGL()) {
+  // use Tangram to draw tiles when there is WebGL available on the browser
+  var layer = Tangram.leafletLayer({
+    scene: 'https://cdn.rawgit.com/tangrams/refill-style/gh-pages/refill-style.yaml',
+    attribution: '<a href="https://mapzen.com/tangram" target="_blank">Tangram</a> | &copy; OSM contributors | <a href="https://mapzen.com/" target="_blank">Mapzen</a>'
+  });
+  layer.addTo(map);
+} else {
+  // Use normal OSM tiles instead of Tangram when there is no webgl available
+  L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+   }).addTo(map);
+}
+
+
+// detect webgl on browser for Tangram
+function hasWebGL() {
+  try {
+    var canvas = document.createElement('canvas');
+    return !!(window.WebGLRenderingContext && (canvas.getContext('webgl') || canvas.getContext('experimental-webgl')));
+  } catch (x) {
+    return false;
+  }
+}
+ 
+ var demo = {
+  costing: 'multimodal',
+  date_time: {
+    type: 1, //depart at
+    value: getNextTuesday() + 'T08:00' // Turn-by-turn supports depart time for transit routing. For demo we wanted to be consistent and always use Next Tuesday at 8am
+  }
+}
+
+/**
+ * Returns a string of next Tuesday's date based
+ * on the current day of the week.  If today is Tuesday,
+ * then we use the following Tuesday's date.
+ *
+ * @returns {string} in format of 'YYYY-MM-DD'
+ */
+function getNextTuesday () {
+  var today = new Date(), day, tuesday;
+  day = today.getDay();
+  tuesday = today.getDate() - day + (day === 0 ? -6 : 2);
+  tuesday += 7;
+  today.setDate(tuesday);
+  return today.toISOString().split('T')[0];
+}
+
+var control = L.Routing.control({
+  routeLine: function (route, options) { return L.Routing.mapzenLine(route, options); },
+  waypoints: [
+    L.latLng(41.477115, -81.791142),
+    L.latLng(41.49417, -81.68317)
+  ],
+  // You can get your own Mapzen turn-by-turn & search API key from the Mapzen developer portal (https://mapzen.com/developers/)
+  geocoder: L.Control.Geocoder.mapzen('valhalla-DRrUBwt'),
+  reverseWaypoints: true,
+  router: L.Routing.mapzen('valhalla-DRrUBwt', demo),
+  formatter: new L.Routing.mapzenFormatter(),
+  summaryTemplate:'<div class="start">{name}</div><div class="info {costing}">{distance}, {time}</div>'
+}).addTo(map);
+
+L.Routing.errorControl(control).addTo(map);
